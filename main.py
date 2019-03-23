ed1f379a643dbe74b6ce7850b73d7fe451de98d9
Karthikeyan Jaikumar <karthikeyan.jaikumar@gmail.com>
2019-03-23 00:51:30 -0400

    Minor changes

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
-import pyspark	
-from src.pre_process import call_me
 from pyspark import SparkContext,SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 
-def getCounts(df):
-    df.write.parquet("hello")
-    return(df.count())  
+from src.pre_process import getCounts
 
 def main(sqlContext):
-
-  df=sqlContext.read.text("sample.txt")
-  df.show()
-  total_rec = getCounts(df)
-  print("Number of records :",total_rec)
+   df = sqlContext.read.text("sample.txt")
+   df.show()
+   total_rec = getCounts(df)
+   print("Number of records :", total_rec)
 
 if __name__=='__main__':
    conf = (SparkConf().setAppName('Application name'))
--- src/pre_process.py
+++ src/pre_process.py
@@ -1,2 +1,8 @@
 def call_me():
    print("Hello Spark")
+
+def getCounts(df):
+   df.write.parquet("hello")
+   call_me()
+   return (df.count())
+
